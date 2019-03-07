6d243dc72e6d0a2c4c3c65b4b6f50e3d7e9a75da
Raleigh Wayland <raleigh.wayland@greenseedtech.com>
2019-03-06 22:01:35 -0700

    resetting body styles after LayoutEffect unmounts

--- src/components/hook-examples/LayoutEffect.js
+++ src/components/hook-examples/LayoutEffect.js
@@ -3,9 +3,15 @@ import React, {useEffect, useLayoutEffect} from 'react';
 const LayoutEffect = () => {
   useEffect(() => {
     document.body.style.backgroundColor = '#f00';
+    return () => {
+      document.body.style.backgroundColor = null;
+    };
   }, []);
   useLayoutEffect(() => {
     document.body.style.backgroundColor = '#0f0';
+    return () => {
+      document.body.style.backgroundColor = null;
+    };
   }, []);
   return (
     <div>
